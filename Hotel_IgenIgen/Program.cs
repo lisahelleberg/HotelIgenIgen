fdbaf96f88612a403291d62aacd32e4c5c74a2d5
Lisa Vind Visby Helleberg <lisa4619@edu.easj.dk>
2017-03-15 12:54:10 +0100

    Lavet metode ListAlleKunder()

--- Hotel_IgenIgen/Program.cs
+++ Hotel_IgenIgen/Program.cs
@@ -10,7 +10,8 @@ namespace Hotel_IgenIgen
     {
         static void Main(string[] args)
         {
-            ListAlleHoteller();
+            //ListAlleHoteller();
+            ListAlleKunder();
         }
 
         private static void ListAlleHoteller()
@@ -22,11 +23,24 @@ namespace Hotel_IgenIgen
 
                 foreach (var h in HotelList1)
                 {
-                    Console.WriteLine($"Hotelnummer: {h.Hotel_No}, hotelnavn: {h.Name}, hoteladresse: {h.Address}");
+                    Console.WriteLine($"Hotelnummer: {h.Hotel_No}, Hotelnavn: {h.Name}, Hoteladresse: {h.Address}");
                 }
             }
         }
 
-        
+        private static void ListAlleKunder()
+        {
+            using (var db = new HotelContext())
+            {
+                var HotelList2 = from g in db.Guest
+                                 select g;
+
+                foreach (var g in HotelList2)
+                {
+                    Console.WriteLine($"Gæstenummer: {g.Guest_No}, Navn: {g.Name}, Adresse: {g.Address}");
+                }
+            }
+        }
+
     }
 }
